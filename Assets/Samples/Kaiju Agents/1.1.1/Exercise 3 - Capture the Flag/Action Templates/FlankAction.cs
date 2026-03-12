7056ef5af7ddccbe3854d6cb844b32a70e5a9e24
Julia Ducharme <122840323+jcharme@users.noreply.github.com>
2026-03-12 13:04:57 -0400

    Added flank logic, still have to test to see if it works

--- Assets/Samples/Kaiju Agents/1.1.1/Exercise 3 - Capture the Flag/Action Templates/FlankAction.cs
+++ Assets/Samples/Kaiju Agents/1.1.1/Exercise 3 - Capture the Flag/Action Templates/FlankAction.cs
@@ -6,13 +6,27 @@ using UnityEngine;
 public class FlankAction : KaijuUtilityAction
 {
     [SerializeField] private string targetKey = "TargetEnemy";
-
+    [Tooltip("How far to the side of the enemy to flank.")]
+    [SerializeField] private float sideOffset = 4f;
+    
     public override void Enter([NotNull] KaijuUtilityBrain brain)
     {
         Component enemy = brain.Get<Component>(targetKey);
         if (enemy)
         {
-            // TODO: whatever flank does
+            // Calculate flank position based on enemy's current orientation
+            Vector3 enemyPos = enemy.transform.position;
+            Vector3 enemyRight = enemy.transform.right;
+            Vector3 enemyBack = -enemy.transform.forward;
+            
+            // Target position should be to the side and backwards
+            Vector3 flankPos = enemyPos + (enemyRight * sideOffset) + (enemyBack * 2f);
+            
+            // Move to flank position
+            brain.Agent.PathFollow(flankPos, clear: true);
+            
+            // Keep agent looking at enemy
+            brain.Agent.LookTransform = enemy.transform;
         }
     }
 }
