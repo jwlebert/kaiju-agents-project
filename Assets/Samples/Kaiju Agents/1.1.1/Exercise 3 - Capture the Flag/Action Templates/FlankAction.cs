9c730bf28c29b68a0a4362ba1f72841b85c3983c
jwlebert <j.lebert4402@gmail.com>
2026-03-12 00:52:23 -0400

    added placeholders for flank and patrol

---
+++ Assets/Samples/Kaiju Agents/1.1.1/Exercise 3 - Capture the Flag/Action Templates/FlankAction.cs
@@ -0,0 +1,17 @@
+using KaijuSolutions.Agents.Utility;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FlankAction", menuName = "Scriptable Objects/FlankAction")]
+public class FlankAction : KaijuUtilityAction
+{
+    [SerializeField] private string targetKey = "TargetEnemy";
+
+    public override void Enter([NotNull] KaijuUtilityBrain brain)
+    {
+        Component enemy = brain.Get<Component>(targetKey);
+        if (enemy)
+        {
+            // TODO: whatever flank does
+        }
+    }
+}
